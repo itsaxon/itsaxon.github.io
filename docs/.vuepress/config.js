ef566129d97cf74adb98c9ecef3fb16c0fec80aa
itsaxon <18856655661@163.com>
2024-08-06 15:55:24 +0000

    add md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,34 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   bundler: viteBundler(),
-  theme: defaultTheme(),
+  theme: defaultTheme({
+    navbar: [
+      { text: 'Homepage', link: '/' },
+      { 
+        text: 'About ME', 
+        children: [
+          { text: 'Github', link: 'https://github.com/itsaxon' },
+          { text: 'Gitee', link: 'https://gitee.com/itsaxon' },
+          { text: 'Juejin', link: 'https://juejin.cn/user/1574156383554909' },
+        ]
+      }
+    ],
+    sidebar: [
+      {
+        text: 'Personal Growth',
+        link: '/',
+        collapsible: false // 不折叠
+      },
+      {
+        text: "Core Java",
+        link: '/personal_growth/core_java/Java基础',
+        collapsible: false, // 不折叠
+        children: [
+          { text: "Java基础", link: "/personal_growth/core_java/Java基础" },
+          { text: "Java集合", link: "/personal_growth/core_java/Java集合" }
+        ]
+      }
+    ]
+  }),
   title: "Leo's Personal Wiki",
 })
